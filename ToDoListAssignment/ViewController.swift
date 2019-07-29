5809afc1623746799cff64b6de8de1be7a7c5661
Shubroto <shuvotech25@gmail.com>
2019-07-29 01:55:34 -0500

    added all the files

--- ToDoListAssignment/ViewController.swift
+++ ToDoListAssignment/ViewController.swift
@@ -8,11 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+var listArray: [String] = []
+
+class ViewController: UIViewController {
+    
+    @IBAction func exitOnReturn(_ sender: Any) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+           self.view.endEditing(true)
+        
+    }
+    
+    
+    @IBOutlet weak var GetDataTextField: UITextField!
+    
+    
+    
+    @IBAction func SubmitButtonPressed(_ sender: Any){
+        //userdefault.set array
+   // buttonPressed = true
+        
+       
+       
+        
+        listArray.append(GetDataTextField.text!)
+         UserDefaults.standard.set(listArray, forKey: "name")
+        print("from Button \(listArray)")
+        
+        self.navigationController?.popViewController(animated: true)
+        
+        
+        //ToDoListTableViewCell().CellLabel?.text = "nnnn"
+        
+//        guard sender is ToDoListTableViewCell else {
+//            fatalError("Unexpected sender: \(sender)")
+//        }
+//
+//        submitText.CellLabel.text = GetDataTextField.text
+        
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
 
